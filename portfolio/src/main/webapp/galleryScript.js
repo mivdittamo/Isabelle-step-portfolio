146fb4e0c731bd50ddc3e5cde4258c603c8f2c03
Isabelle Dittamo <mivdittamo@gmail.com>
2020-05-28 15:30:16 -0400

    Ordered css properties and fixed indentation

--- portfolio/src/main/webapp/galleryScript.js
+++ portfolio/src/main/webapp/galleryScript.js
@@ -9,12 +9,12 @@ function moveSlide(n) {
 function displaySlide(n) {
   var slides = document.getElementsByClassName("EuropeSlideshow");
   if (n > slides.length) {
-      slideIndex = 1;
+    slideIndex = 1;
   } else if (n < 1) {
-      slideIndex = slideIndex = slides.length;
+    slideIndex = slideIndex = slides.length;
   }
   for (var i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   slides[slideIndex - 1].style.display = "block";
 }
