85ca2a9ff3d077b9d5336a4c14d3ca7b903c8428
Isabelle Dittamo <mivdittamo@gmail.com>
2020-05-28 13:24:10 -0400

    Added basic slideshow to Gallery

---
+++ portfolio/src/main/webapp/galleryScript.js
@@ -0,0 +1,21 @@
+// Functions for the slideshows in Gallery
+var slideIndex = 1;
+displaySlide(slideIndex);
+
+function moveSlide(n) {
+    displaySlide(slideIndex += n);
+}
+
+function displaySlide(n) {
+    var slides = document.getElementsByClassName("EuropeSlideshow");
+    console.log(slides);
+    if (n > slides.length) {
+        slideIndex = 1;
+    } else if (n < 1) {
+        slideIndex = slideIndex = slides.length;
+    }
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
